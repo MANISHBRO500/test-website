e1430f9c9dbb1377e5b80d06fc2831a6077981a7
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-02-08 20:57:10 +0530

    Delete backend directory

--- backend/models/question.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const questionSchema = new mongoose.Schema({
-    questionText: { type: String, required: true },
-    type: { type: String, enum: ['mcq', 'short', 'long'], required: true },
-    options: [String], // Only for MCQs
-});
-
-module.exports = mongoose.model('Question', questionSchema);
