6d544954ac4e47fbb53bd8352fc36871cbea1785
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-02-08 21:20:34 +0530

    Delete backend directory

--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
-    role: { type: String, enum: ['teacher', 'student'], required: true },
-    password: { type: String, required: true }
-});
-
-module.exports = mongoose.model('User', userSchema);
